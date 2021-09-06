834425b7e9b8198cf719f468f17b2750827fe418
valevqb03 <44345368+valevqb@users.noreply.github.com>
2021-09-06 16:17:24 -0600

    Prueba

---
+++ D3/349116b280ed2fdf@251.js
@@ -0,0 +1,113 @@
+// https://observablehq.com/@d3/zoomable-icicle@251
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  const fileAttachments = new Map([["flare-2.json",new URL("./files/e65374209781891f37dea1e7a6e1c5e020a3009b8aedf113b4c80942018887a1176ad4945cf14444603ff91d3da371b3b0d72419fa8d2ee0f6e815732475d5de",import.meta.url)]]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], function(md){return(
+md`# Zoomable Icicle
+
+This variant of an [icicle diagram](/@mbostock/d3-icicle) shows only three layers of the hierarchy at a time. Click a node to zoom in, or the left column to zoom out. Compare to a [sunburst](/@d3/zoomable-sunburst).`
+)});
+  main.variable(observer("chart")).define("chart", ["partition","data","d3","width","height","color","format"], function(partition,data,d3,width,height,color,format)
+{
+  const root = partition(data);
+  let focus = root;
+
+  const svg = d3.create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .style("font", "10px sans-serif");
+
+  const cell = svg
+    .selectAll("g")
+    .data(root.descendants())
+    .join("g")
+      .attr("transform", d => `translate(${d.y0},${d.x0})`);
+
+  const rect = cell.append("rect")
+      .attr("width", d => d.y1 - d.y0 - 1)
+      .attr("height", d => rectHeight(d))
+      .attr("fill-opacity", 0.6)
+      .attr("fill", d => {
+        if (!d.depth) return "#ccc";
+        while (d.depth > 1) d = d.parent;
+        return color(d.data.name);
+      })
+      .style("cursor", "pointer")
+      .on("click", clicked);
+
+  const text = cell.append("text")
+      .style("user-select", "none")
+      .attr("pointer-events", "none")
+      .attr("x", 4)
+      .attr("y", 13)
+      .attr("fill-opacity", d => +labelVisible(d));
+
+  text.append("tspan")
+      .text(d => d.data.name);
+
+  const tspan = text.append("tspan")
+      .attr("fill-opacity", d => labelVisible(d) * 0.7)
+      .text(d => ` ${format(d.value)}`);
+
+  cell.append("title")
+      .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+  function clicked(event, p) {
+    focus = focus === p ? p = p.parent : p;
+
+    root.each(d => d.target = {
+      x0: (d.x0 - p.x0) / (p.x1 - p.x0) * height,
+      x1: (d.x1 - p.x0) / (p.x1 - p.x0) * height,
+      y0: d.y0 - p.y0,
+      y1: d.y1 - p.y0
+    });
+
+    const t = cell.transition().duration(750)
+        .attr("transform", d => `translate(${d.target.y0},${d.target.x0})`);
+
+    rect.transition(t).attr("height", d => rectHeight(d.target));
+    text.transition(t).attr("fill-opacity", d => +labelVisible(d.target));
+    tspan.transition(t).attr("fill-opacity", d => labelVisible(d.target) * 0.7);
+  }
+  
+  function rectHeight(d) {
+    return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+  }
+
+  function labelVisible(d) {
+    return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+  }
+  
+  return svg.node();
+}
+);
+  main.variable(observer("data")).define("data", ["FileAttachment"], function(FileAttachment){return(
+FileAttachment("flare-2.json").json()
+)});
+  main.variable(observer("partition")).define("partition", ["d3","height","width"], function(d3,height,width){return(
+data => {
+  const root = d3.hierarchy(data)
+      .sum(d => d.value)
+      .sort((a, b) => b.height - a.height || b.value - a.value);  
+  return d3.partition()
+      .size([height, (root.height + 1) * width / 3])
+    (root);
+}
+)});
+  main.variable(observer("color")).define("color", ["d3","data"], function(d3,data){return(
+d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+)});
+  main.variable(observer("format")).define("format", ["d3"], function(d3){return(
+d3.format(",d")
+)});
+  main.variable(observer("width")).define("width", function(){return(
+975
+)});
+  main.variable(observer("height")).define("height", function(){return(
+1200
+)});
+  main.variable(observer("d3")).define("d3", ["require"], function(require){return(
+require("d3@6")
+)});
+  return main;
+}
